c64ab5b757b46f99c0477f78db8ce951e4ff6951
nhiph <nhiph@BLACKWIDOW>
2025-09-02 09:27:34 -0500

    Finish part 4

--- Number Guessing Game/Program.cs
+++ Number Guessing Game/Program.cs
@@ -1,47 +1,72 @@
 ﻿//generate a random integer between 1 and 5
 using System;
 
-//Datatype variable = value;
-Random rand = new Random();
-int randomNumber = rand.Next(1, 6); // generates a number between 1 and 5
-int guess;
-int numberOfguessed = 0;
-do
+Console.WriteLine("Welcome to our game!");
+
+Console.WriteLine("Please input the the lower bound value (lower values that could be guessed)? <<");
+string answer = Console.ReadLine();
+int lowerBound = int.Parse(answer);
+
+Console.WriteLine("Please input the the upper bound value (highest values that could be guessed)? <<");
+answer = Console.ReadLine();
+int upperBound = int.Parse(answer);
+
+for (int i = 0; i < int.MaxValue; i++)
 {
-    Console.WriteLine("Please guess a number between 1 and 5: ");
-    string usersGuests = Console.ReadLine();
-    guess = int.Parse(usersGuests);
-    //guess = Convert.ToInt32(usersGuests);
-    numberOfguessed++;
 
-    Console.WriteLine($"You guessed {usersGuests}");
+    //Datatype variable = value;
+    Random rand = new Random();
+    int randomNumber = rand.Next(lowerBound, upperBound + 1); // generates a number between 1 and 5
+    int guess;
 
-   // if (randomNumber == guess)
-    //{
-    //     Console.WriteLine("Congratulations, you guessed correctly!");
-    //}
-    //else
-   // {
-   //     Console.WriteLine("Sorry, you guessed wrong.");
-    //}
-    if (randomNumber != guess)
+    int numberOfguessed = 0;
+
+    do
     {
-        if (randomNumber > guess)
+        Console.WriteLine($"Please guess a number between {lowerBound} and {upperBound}: ");
+        string usersGuests = Console.ReadLine();
+        guess = int.Parse(usersGuests);
+        //guess = Convert.ToInt32(usersGuests);
+        numberOfguessed++;
+
+        Console.WriteLine($"You guessed {usersGuests}");
+
+        // if (randomNumber == guess)
+        //{
+        //     Console.WriteLine("Congratulations, you guessed correctly!");
+        //}
+        //else
+        // {
+        //     Console.WriteLine("Sorry, you guessed wrong.");
+        //}
+        if (randomNumber != guess)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Too low, try again.");
-            Console.ForegroundColor = ConsoleColor.White;
+            if (randomNumber > guess)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Too low, try again.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Too high, try again.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Too high, try again.");
-            Console.ForegroundColor = ConsoleColor.White;
-        }   
+
+    } while (randomNumber != guess);
+
+    Console.WriteLine($"Congratulations, you guessed correctly in {numberOfguessed} attempts!");
+
+    Console.WriteLine($"We generated the random number {randomNumber}");
+
+    Console.WriteLine("Do you want to play again? (yes/no)");
+    answer = Console.ReadLine();
+
+    if (answer != "yes") // answer == "no"
+    {
+        break;
     }
 
-} while (randomNumber != guess);
-
-Console.WriteLine("Congratulations, you guessed correctly!");
-
-Console.WriteLine($"We generated the random number {randomNumber}");
+}
